142fe506f9dd760ba86ea5abee3376218ee48ff2
Oliasinkevich <olga.sinkevich89@gmail.com>
2023-09-21 16:57:15 +0300

    Vue CLI: homework

---
+++ 4 Vue CLI/homework/my-project/pages/HomeComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <!-- <LineComponent /> -->
+        <HeaderComponent />
+
+    </div>
+</template>
+
+<script>
+// import LineComponent from '@/components/LineComponent.vue';
+import HeaderComponent from '@/components/HeaderComponent.vue';
+
+export default {
+    name: 'HomeComponent',
+    components: { 
+        // LineComponent,
+        HeaderComponent
+     }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
